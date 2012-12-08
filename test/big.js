0d4e93c0ad3651f95267b4b2540bc5299070e16d
Dominic Tarr <dominic.tarr@gmail.com>
2012-12-07 21:14:37 -0800

    heavy tests, for "benchmarking"

---
+++ test/big.js
@@ -0,0 +1,52 @@
+var a = require('..')
+var tape = require('tape')
+
+function test(n, times) {
+
+tape ('big '+n+'x'+times, function (t) {
+
+  var start = Date.now()
+  var big = [], l = n
+
+  function random() {
+    return 'hello'+Math.random()
+  }
+
+  while(l--)
+    big.push(random())
+
+  function randomChange(big) {
+    var _big = big.slice()
+    //if (Math.random() < 0.3) {
+      var insert = []
+      while(Math.random() < 0.7)
+        insert.push(random())
+    
+      insert.unshift(~~(Math.random()*insert.length*2)) //deletes
+      insert.unshift(~~(Math.random()*big.length))     //insert index
+      _big.splice.apply(_big, insert)
+  //  }
+    return _big
+  }
+
+  while(times --) {
+    var _big = randomChange(big)
+    var p = a.diff(big, _big)
+    t.deepEqual(_big, a.patch(big, p, true))
+  }
+
+  console.log('TIME('+n+') = ', n/(Date.now() - start))
+  t.end()
+})
+
+}
+
+test(10  , 10)
+test(100 , 10)
+test(200 , 10)
+test(500 , 10)
+test(600 , 10)
+test(800 , 10)
+test(1000, 10)
+test(10000, 10)
+
