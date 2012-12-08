00da741f87bd5574b2ce7ef072aa80056d6735ce
Dominic Tarr <dominic.tarr@gmail.com>
2012-12-07 21:11:50 -0800

    split out simplest tests

---
+++ test/2way.js
@@ -0,0 +1,36 @@
+var tape = require('tape')
+var d = require('../')
+
+  function split(a) {
+    if('string' === typeof a)
+      return a.split('')
+    return a
+  }
+
+  function test (a, b, lcs) {
+    tape('lcs '+a+' '+b+' == '+lcs, function (assert) {
+      a = split(a)
+      b = split(b)
+      lcs = split(lcs)
+      var _lcs = d.lcs(a, b)
+      d.chunk([a, b], console.log)
+      assert.deepEqual(_lcs, lcs)
+      var changes = d.diff(a,b)
+      console.log('PATCH', changes)
+      var newA = d.patch(a, changes)
+      assert.deepEqual(newA, b)
+      assert.end()
+    })
+  }
+
+  test('AA', 'AA', 'AA')
+  test('AB', 'BA', 'A')
+  test('ABA', 'BAA', 'AA')
+  test('TANYANA', 'BANANA', 'ANANA')
+  // the naive model takes 2.5 seconds to find this:
+  // time to optimise...
+  test('aoenuthooao', 'eukmcybkraoaeuo', 'aoeuo')
+  test('aoenuthooaeuoao', 'eukipoimcybkraoaeuo', 'euooaeuo')
+  // added caching... now it's way faster.
+
+
--- test/test.js
+++ test/test.js
@@ -1,38 +1,11 @@
 var tape = require('tape')
 var d = require('../')
 
-
-  var assert = require('assert')
   function split(a) {
     if('string' === typeof a)
       return a.split('')
     return a
   }
-
-  function test (a, b, lcs) {
-    tape('lcs '+a+' '+b+' == '+lcs, function (assert) {
-      a = split(a)
-      b = split(b)
-      lcs = split(lcs)
-      var _lcs = d.lcs(a, b)
-      d.chunk([a, b], console.log)
-      assert.deepEqual(_lcs, lcs)
-      var changes = d.diff(a,b)
-      var newA = d.patch(a, changes)
-      assert.deepEqual(newA, b)
-      assert.end()
-    })
-  }
-
-  test('AA', 'AA', 'AA')
-  test('AB', 'BA', 'A')
-  test('ABA', 'BAA', 'AA')
-  test('TANYANA', 'BANANA', 'ANANA')
-  // the naive model takes 2.5 seconds to find this:
-  // time to optimise...
-  test('aoenuthooao', 'eukmcybkraoaeuo', 'aoeuo')
-  test('aoenuthooaeuoao', 'eukipoimcybkraoaeuo', 'euooaeuo')
-  // added caching... now it's way faster.
 
   function test3way(args, expected) {
     tape('diff3 '+args.join(' ')+' === '+expected, function (assert) {
@@ -69,12 +42,14 @@ var d = require('../')
   console.log(d.diff3(split("ABCXYZF"), split("ABDCEF"), split("AXBCEFG")))
 
   function assertDiffPatch (a, b) {
+    tape('assertDiffPatch', function (assert) {
     a = split(a)
     b = split(b)
     var patch = d.diff(a, b)
     var patched = d.patch(a, JSON.parse(JSON.stringify(patch)))
 
     assert.deepEqual(b, patched)
+    })
 
   }
 
